0b7c7f1da232b6b2ac01d64ba555b8676c774f16
mohasinSH <150676373+mohasinSH@users.noreply.github.com>
2024-10-21 04:38:31 +0530

    Update CreateCustomer.js

--- src/CreateCustomer.js
+++ src/CreateCustomer.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { ToastContainer, toast } from 'react-toastify'; // Import toast components
-import 'react-toastify/dist/ReactToastify.css'; // Import toast styles
+import { ToastContainer, toast } from 'react-toastify'; 
+import 'react-toastify/dist/ReactToastify.css'; 
 
 const CreateCustomer = () => {
   const [companyName, setCompanyName] = useState('');
